45941709d8d4eebb21ae0c5afdc9b3b30ad62828
Luis <softwarelaycho@gmail.com>
2019-11-27 20:19:00 -0300

    reservas

---
+++ app/src/main/java/py/com/eko/fisiocenter/Reservas.java
@@ -0,0 +1,14 @@
+package py.com.eko.fisiocenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reservas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservas);
+    }
+}
